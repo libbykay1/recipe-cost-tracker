43cd12a31f11f5d2d95c3194fa12df6d9588ad8b
Libby Eilbert <elizabeth.eilbert@gmail.com>
2023-08-02 13:58:11 -0700

    nav and recipe list css

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -7,13 +7,13 @@ function Nav() {
         <div className={styles.headerlogo}>
             <img className={styles.logo} src="https://sweetdragonbaking.com/cdn/shop/files/sd_logo_black-xlg_400x200@2x.png?v=1613181341" />
         </div>
-        <div>
-            <ul>
-                <li>
-                    <NavLink to="/recipes/">Recipes</NavLink>
+        <div className={styles.container}>
+            <ul className={styles.navbarlist}>
+                <li className={styles.navitem}>
+                    <NavLink className={styles.links} to="/recipes/">Recipes</NavLink>
                 </li>
-                <li>
-                    <NavLink to="/recipes/new/">New Recipe</NavLink>
+                <li className={styles.navitem}>
+                    <NavLink className={styles.links} to="/recipes/new/">New Recipe</NavLink>
                 </li>
             </ul>
         </div>
--- ghi/app/src/recipes/RecipeList.js
+++ ghi/app/src/recipes/RecipeList.js
@@ -3,13 +3,12 @@ import styles from './styles.module.css'
 function RecipeList({recipes}) {
     return (
         <>
-        <h1 className={styles.bigblue}>Recipes</h1>
-        <ul>
+        <ul className={styles.recipelist}>
             {recipes.map(recipe => {
                 const recipeUrl = `http://localhost:3000/recipes/${recipe.id}/view`
                 return (
-                    <li key={recipe.id}>
-                        <a href={recipeUrl}>{recipe.name}</a>
+                    <li className={styles.listitem} key={recipe.id}>
+                        <a className={styles.links} href={recipeUrl}>{recipe.name}</a>
                         </li>
                 )
             })}
